8b382b141a14581307dc3fa06e8b551a1dd639f4
MR-Bhavan <bhavankumar8003@gamil.com>
2024-03-17 18:29:08 +0530

    import

---
+++ new/src/data/constant.js
@@ -0,0 +1,160 @@
+export const Bio = {
+  name: "Bhavan",
+  roles: ["Front End Developer", "React JS Developer", "Javascript Developer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/MR-Bhavan",
+  resume:
+    "https://drive.google.com/file/d/1DHSClPstCTGecnq_Ev1ud6dWyXVkPRqg/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/bhavan2003",
+  email:"mailto:bhavankumar803@gmail.com",
+  phone:"tel:+91 9345467714",
+  Instagram:"https://www.instagram.com/__mr_black_bird__"
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React JS",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      // {
+      //   name: "Redux",
+      //   image:
+      //     "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      // },
+      // {
+      //   name: "jQuery",
+      //   image:
+      //     "https://cdn.iconscout.com/icon/free/png-512/free-jquery-8-1175153.png?f=webp&w=256",
+      // },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      // {
+      //   name: "Netlify",
+      //   image:
+      //     "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      // },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.svgrepo.com/show/354202/postman-icon.svg",
+      },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://saasc10.com/psiber/image/Gallery/syed%20ammal.jpg",
+    school: "Syed ammal arts and science collage,Ramanathapuram",
+    date: "June 2020 - June 2023",
+    grade: "6.4 CGPA , 64%",
+    desc: "I Completed my Bachelor's degree in business administration at Syed ammal arts and science collage, Ramanathapuram. I have completed 6 semesters and have a CGPA of 6.4.",
+    degree: "Bachelor of business administration - BBA ",
+  },
+  {
+    id: 1, 
+    img: "https://www.joonsquare.com/usermanage/image/business/velumanickam-mont-matric-hr-sec-school-ramanathapuram-10347/velumanickam-mont-matric-hr-sec-school-ramanathapuram-logo.jpg",
+    school: "Velumanickam mont matric hr sec school ,Ramanathapuram",
+    date: "Apr 2019 - Apr 2020",
+    grade: "65%",
+    desc: "I completed my class 12 high school education at Velumanickam mont matric hr sec school ,Ramanathapuram, where I studied business Maths with Arts.",
+    degree: "HSC(XII),Arts",
+  },
+  {
+    id: 2,
+    img: "2wCEAAoHCBEQFRAPDw8QERERDxEQDxIRDxEPDw8RGBQZGhgUGBgcIS4zHB4rHxgZJjgmKy8xNTU1HiQ7QDszPy40NTEBDAwMEA8QHhISHjQhJCs0NzQ0NDQ0NDQxMTQ0MTQ0NDQxNDQ0NDQ0NDQ0NDQ0MTQ0NDQ0NDQ0NDQ0NDQ0NDQ0NP",
+    school: "Housing board matric high School, Ramanathapuram",
+    date: "Apr 2017 - Apr 2018",
+    grade: "65%",
+    desc: "I completed my class 10 education at Housing board matric high School, Ramanathapuram.",
+    degree: "SSLC(X)"
+  },
+];
+
+export const projects = [
+  // {
+  //   id: 1,
+  //   title: "Weather App",
+  //   date: "Dec 2023",
+  //   description:
+  //     "Developed weather app using Open Weather API to fetch data, dynamically binding it to create an interactive UI for real-time weather updates.",
+  //   image:
+  //     "https://i.ibb.co/DV8zqy2/weatherapp-image.png",
+  //   tags: [
+  //     "HTML",
+  //     "CSS",
+  //     "Javascript",
+  //     "React JS",
+  //   ],
+  //   github: "https://github.com/veeravelS/Weather--app.git",
+  //   webapp: "https://veeravels.github.io/Weather--app/",
+  // },
+  {
+    id: 2,
+    title: "Personal Portfolio Website",
+    date: "Jan 2024",
+    description:
+      "Developed a Dynamic personal portfolio website to showcase skills,project and professional background ,providing comprehensive overview of expertise in web development .",
+    image:
+      "https://i.ibb.co/yqkj7Xn/portfolio.png",
+    tags: ["HTML", "CSS", "Javascript", "Bootstrap", "React JS","Styled Components"],
+    github: "https://github.com/veeravelS/Personal-portfolio-Website.git",
+    webapp: " https://veeravels.github.io/Personal-portfolio-Website/",
+  },
+  // {
+  //   id: 3,
+  //   title: "Form Validation using jQuery",
+  //   date: "Jan 2024",
+  //   description:
+  //     "This dynamic project enhances user experience by implementing robust client-side validation, ensuring accurate and error-free data submissions. It reflects my skills in front-end development.",
+  //   image:
+  //     "https://i.ibb.co/8Xc13XC/Form-validation-image.png",
+  //   tags: ["HTML", "CSS", "Javascript", "Bootstrap", "jQuery"],
+  //   github: "https://github.com/veeravelS/Form-validation.git",
+  //   webapp: "https://veeravels.github.io/Form-validation/",
+  // }
+];
